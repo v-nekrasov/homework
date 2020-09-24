4c915927471ade8130696bd73a6f1346c4f02c76
Nekrasov-VV <vsevolod.nekrasov@math.msu.ru>
2020-09-24 18:06:28 +0300

    Create 1.cpp

---
+++ 3rd_semester/Seminars/1.cpp
@@ -0,0 +1,35 @@
+﻿//Задача к экзамену
+#include<vector>
+#include<list>
+#include<stdio.h>
+#include<stdlib.h>
+#include<iostream>
+using namespace std;
+
+vector< list<int> > create (FILE *in, int k)
+{
+	vector< list<int> > a(k); int n;
+	while(fscanf(in, "%d", &n)==1)
+		a[n%k].push_back(n);
+	return a;		
+}
+int main()
+{
+	FILE *in=fopen("3.txt", "r"); int k;
+	cout<<"k=";
+	cin>>k;
+	vector< list<int> > a=create(in, k);
+	fclose(in);
+	cout<<"result"<<endl;
+	vector< list<int> >::iterator t1;
+	list<int>::iterator t2;
+	for(t1=a.begin();t1!=a.end(); t1++)
+	{
+		for(t2=(*t1).begin(); t2!=(*t1).end(); t2++)
+			printf("%d ", *t2);
+		printf("\n");
+	}		
+
+ 
+	return 0;
+}
